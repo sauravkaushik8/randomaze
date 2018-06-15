d61b9c03d90d08bce1658fc3b278c53e7e321393
Saurav Kaushik <sauravkaushik8@gmail.com>
2018-06-16 01:54:16 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'randomaze',
+  packages = ['randomaze'],
+  version = '0.1.0',
+  description = 'A Random Maze framework to build reinforcent learning players on top of.',
+  author = 'Saurav Kaushik',
+  author_email = 'sauravkaushik8@gmail.com',
+  url = 'https://github.com/sauravkaushik8/randomaze',
+  download_url = 'https://github.com/sauravkaushik8/randomaze/archive/1.0.tar.gz',
+  keywords = ['Reinforcent Learning', 'Maze', 'Game Theory'],
+  classifiers = [],
+)
